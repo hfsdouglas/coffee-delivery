9e9c1deb1c0bf7cb61415d66b99c757e89ff42e9
Douglas Faria <hfsdouglas@gmail.com>
2025-03-01 00:31:54 -0300

    feat: add address form and payment radio card group, selected coffees card

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,3 +1,173 @@
+import {
+  Bank,
+  CreditCard,
+  CurrencyDollar,
+  MapPinLine,
+  Money,
+} from "@phosphor-icons/react";
+import { QuantitySelector } from "../../components/QuantitySelector";
+import {
+  CardHeader,
+  CheckoutForm,
+  CoffeesCard,
+  FormCard,
+  FormContainer,
+  Input,
+  RadioCardContainer,
+  RadioCardIndicator,
+  RadioCardItem,
+  Table,
+} from "./styles";
+import { useTheme } from "styled-components";
+
 export function Checkout() {
-  return <h1>Checkout</h1>;
+  const theme = useTheme();
+
+  return (
+    <CheckoutForm>
+      <FormContainer>
+        <h3>Complete seu pedido</h3>
+
+        <FormCard>
+          <CardHeader>
+            <MapPinLine size={20} color={theme["yellow-dark"]} />
+
+            <div>
+              <h4>Endereço de Entrega</h4>
+              <span>Informe o endereço onde deseja receber seu pedido</span>
+            </div>
+          </CardHeader>
+
+          <Table>
+            <thead>
+              <tr>
+                <th style={{ minWidth: "200px" }} />
+                <th style={{ width: "100%" }} />
+                <th style={{ minWidth: "60px" }} />
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr>
+                <td>
+                  <Input type="text" placeholder="CEP" />
+                </td>
+              </tr>
+
+              <tr>
+                <td colSpan={3}>
+                  <Input type="text" placeholder="Endereço" />
+                </td>
+              </tr>
+
+              <tr>
+                <td>
+                  <Input type="text" placeholder="Número" />
+                </td>
+
+                <td colSpan={2}>
+                  <Input type="text" placeholder="Complemento (Opcional)" />
+                </td>
+              </tr>
+
+              <tr>
+                <td>
+                  <Input type="text" placeholder="Bairro" />
+                </td>
+
+                <td>
+                  <Input type="text" placeholder="Cidade" />
+                </td>
+
+                <td>
+                  <Input type="text" placeholder="UF" />
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+        </FormCard>
+
+        <FormCard>
+          <CardHeader>
+            <CurrencyDollar size={20} color={theme.purple} />
+
+            <div>
+              <h4>Pagamento</h4>
+              <span>
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </span>
+            </div>
+          </CardHeader>
+
+          <RadioCardContainer>
+            <RadioCardItem value="credito" id="credito">
+              <div>
+                <CreditCard size={16} color={theme.purple} />
+                <label htmlFor="credito">CARTÃO DE CRÉDITO</label>
+              </div>
+
+              <RadioCardIndicator />
+            </RadioCardItem>
+
+            <RadioCardItem value="debito" id="debito">
+              <div>
+                <Bank size={16} color={theme.purple} />
+                <label htmlFor="debito">CARTÃO DE DÉBITO</label>
+              </div>
+
+              <RadioCardIndicator />
+            </RadioCardItem>
+
+            <RadioCardItem value="dinheiro" id="dinheiro">
+              <div>
+                <Money size={16} color={theme.purple} />
+                <label htmlFor="dinheiro">DINHEIRO</label>
+              </div>
+
+              <RadioCardIndicator />
+            </RadioCardItem>
+          </RadioCardContainer>
+        </FormCard>
+      </FormContainer>
+
+      <FormContainer>
+        <h3>Cafés selecionados</h3>
+
+        <CoffeesCard>
+          <main>
+            <section>
+              <img src="" alt="" />
+
+              <div>
+                <h4>Expresso Tradicional</h4>
+                <div>
+                  <QuantitySelector />
+                  <button type="button">Remover</button>
+                </div>
+              </div>
+            </section>
+          </main>
+
+          <footer>
+            <div>
+              <span>Total de itens</span>
+              <span>R$ 29,70</span>
+            </div>
+
+            <div>
+              <span>Entrega</span>
+              <span>R$ 3,50</span>
+            </div>
+
+            <div>
+              <strong>Total</strong>
+              <strong>R$ 33,20</strong>
+            </div>
+
+            <button type="submit">CONFIRMAR PEDIDO</button>
+          </footer>
+        </CoffeesCard>
+      </FormContainer>
+    </CheckoutForm>
+  );
 }
---
+++ src/pages/Checkout/styles.ts
@@ -0,0 +1,146 @@
+import { RadioGroup } from 'radix-ui'
+import styled from 'styled-components'
+
+export const CheckoutForm = styled.form`
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    gap: 2rem;
+`
+
+export const FormContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+
+    h3 {
+        font-family: 'Baloo 2';
+        font-size: 1.125rem;
+        font-weight: bold;
+    }
+`
+export const FormCard = styled.section`
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+
+    background-color: ${props => props.theme['base-card']};
+    border-radius: 6px;
+    padding: 2.5rem;
+`
+
+export const CoffeesCard = styled.div`
+    background-color: ${props => props.theme['base-card']};
+
+    border-radius: 6px;
+    border-top-right-radius: 36px;
+    border-bottom-left-radius: 36px;
+
+    padding: 2.5rem;
+`
+
+export const CardHeader = styled.header`
+    display: flex;
+    gap: 0.5rem;
+
+    svg: {
+        line-height: 0;
+    }
+
+    div {
+        display: flex;
+        flex-direction: column;
+
+        h4 {
+            font-weight: 400;
+            font-size: 1rem;
+            color: ${props => props.theme['base-subtitle']};
+        }
+
+        span {
+            font-size: 0.875rem;
+            color: ${props => props.theme['base-text']};
+        }
+    }
+`
+
+export const Table = styled.table`
+    border-spacing: 0.5rem;
+    margin-top: -16px;
+`
+
+export const RadioCardContainer = styled(RadioGroup.Root)`
+    display: flex;
+    justify-content: space-between;
+    gap: 0.75rem;
+
+    & *:focus {
+        box-shadow: none;
+    }
+`
+
+export const RadioCardItem = styled(RadioGroup.Item)`
+    position: relative;
+
+    width: 100%;
+    height: 50px;
+
+    border: none;
+    border-radius: 6px;
+
+    background-color: ${props => props.theme['base-button']};
+    
+    [data-state = "checked"]{
+        background-color: ${props => props.theme['purple-light']};
+        border: 1px solid ${props => props.theme['purple']};
+    }
+
+    div {
+        display: flex;
+        align-items: center;
+        gap: 0.75rem;
+
+        padding: 1rem 1.25rem;
+
+        label {
+            color: ${props => props.theme['base-text']};
+            font-size: 0.75rem;
+        }
+
+        width: 100%;
+        height: 100%;
+
+        position: absolute;
+        top: 0;
+        z-index: 1;
+    }
+`
+
+export const RadioCardIndicator = styled(RadioGroup.Indicator)`
+    width: 100%;
+    height: 100%;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+
+    border-radius: 6px;
+`
+
+export const Input = styled.input`
+    width: 100%;
+    height: 42px;
+
+    border: 1px solid ${props => props.theme['base-button']};
+    border-radius: 6px;
+
+    background-color: ${props => props.theme['base-input']};
+    color: ${props => props.theme['base-text']};
+
+    font-size: 0.875rem;
+
+    padding: 0.75rem;
+
+    &::placeholder {
+        color: ${props => props.theme['base-label']};
+    }
+`
