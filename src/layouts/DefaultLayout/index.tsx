be1a2538f2853f468607980d95063cec0ffeb41d
Douglas Faria <hfsdouglas@gmail.com>
2025-03-03 16:44:36 -0300

    feat: add CartProvider in the default layout

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,15 +1,16 @@
 import { Outlet } from "react-router-dom";
 import { LayoutContainer } from "./styles";
 import { Header } from "../../components/Header";
+import { CartProvider } from "../../contexts/CartContext";
 
 export function DefaultLayout() {
   return (
-    <>
+    <CartProvider>
       <Header />
 
       <LayoutContainer>
         <Outlet />
       </LayoutContainer>
-    </>
+    </CartProvider>
   );
 }
